09fa23fdd042693fab978ad780229f3ced152e9b
Albina Dobrova <adobrova@docdoc.ru>
2022-09-18 23:46:28 +0500

    fixed conditions again

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -7,15 +7,11 @@ import java.util.Random;
 public class ExampleTest {
     @Test
     void exampleTest0(){
-
-        Assertions.assertTrue(2>=1);
-
+        Assertions.assertTrue( new Random().nextInt() > 4);
     }
 
     @Test
     void exampleTest1(){
-
-        Assertions.assertTrue(3>=2);
-
+        Assertions.assertTrue(new Random().nextInt() > 6);
     }
 }
